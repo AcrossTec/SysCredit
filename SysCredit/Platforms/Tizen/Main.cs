f740e34b4a69ea8046796be57ee3a61e75780641
Denis West <DenisWest@outlook.com>
2023-07-07 23:01:34 -0600

    feat: Creating the main project SysCredit

---
+++ SysCredit/Platforms/Tizen/Main.cs
@@ -0,0 +1,18 @@
+namespace SysCredit
+{
+    using Microsoft.Maui;
+    using Microsoft.Maui.Hosting;
+
+    using System;
+
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
