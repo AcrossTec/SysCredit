ef30ada025e192bcff8609d437ff0ba040079394
Denis West <DenisWest@outlook.com>
2023-07-07 23:01:34 -0600

    feat: Creating the main project SysCredit

---
+++ SysCredit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SysCredit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SysCredit/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SysCredit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
