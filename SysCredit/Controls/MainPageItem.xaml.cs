c0bc3f691c308797d97bef63c2ba79d326708576
Denis West <DenisWest@outlook.com>
2023-07-15 07:37:35 -0600

    feat: New MainPageItem features

--- SysCredit/Controls/MainPageItem.xaml.cs
+++ SysCredit/Controls/MainPageItem.xaml.cs
@@ -10,8 +10,9 @@ public partial class MainPageItem : ContentView
     public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(MainPageItem), Colors.White);
     public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(MainPageItem));
     public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(MainPageItem));
+    public static readonly BindableProperty ItemBackColorProperty = BindableProperty.Create(nameof(ItemBackColor), typeof(Color), typeof(MainPageItem));
     public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(MainPageItem), false);
-    public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(MainPageItem), 5f);
+    public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(MainPageItem), 12f);
 
     public MainPageItem()
     {
@@ -46,6 +47,12 @@ public partial class MainPageItem : ContentView
     {
         get => (Color?)GetValue(BorderColorProperty);
         set => SetValue(BorderColorProperty, value);
+    }
+
+    public Color? ItemBackColor
+    {
+        get => (Color?)GetValue(ItemBackColorProperty);
+        set => SetValue(ItemBackColorProperty, value);
     }
 
     public float CornerRadius
---
+++ SysCredit/Views/Clients/NewClientPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SysCredit.Views.Clients;
+
+public partial class NewClientPage : ContentPage
+{
+	public NewClientPage()
+	{
+		InitializeComponent();
+	}
+}
