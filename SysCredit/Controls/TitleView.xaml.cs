b10798dbbb498a5d10679e9aa613f6d2fb74f21f
Denis West <DenisWest@outlook.com>
2023-07-17 14:10:31 -0600

    feat: Creating NewClientView, Part 1

--- SysCredit/Controls/TitleView.xaml.cs
+++ SysCredit/Controls/TitleView.xaml.cs
@@ -5,6 +5,9 @@ public partial class TitleView : ContentView
     public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TitleView), string.Empty);
     public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(TitleView), Colors.Black);
     public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create(nameof(FontFamily), typeof(string), typeof(TitleView), "InterBold");
+    public static readonly BindableProperty FontAttributesProperty = BindableProperty.Create(nameof(FontAttributes), typeof(FontAttributes), typeof(TitleView), FontAttributes.Bold);
+    public static readonly BindableProperty LineHeightProperty = BindableProperty.Create(nameof(LineHeight), typeof(double), typeof(TitleView), 30.0);
+    public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(TitleView), 24.0);
 
     public TitleView()
     {
@@ -27,5 +30,22 @@ public partial class TitleView : ContentView
     {
         get => (string)GetValue(FontFamilyProperty);
         set => SetValue(FontFamilyProperty, value);
+    }
+    public FontAttributes FontAttributes
+    {
+        get => (FontAttributes)GetValue(FontAttributesProperty);
+        set => SetValue(FontAttributesProperty, value);
+    }
+
+    public double FontSize
+    {
+        get => (double)GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty, value);
+    }
+
+    public double LineHeight
+    {
+        get => (double)GetValue(LineHeightProperty);
+        set => SetValue(LineHeightProperty, value);
     }
 }
