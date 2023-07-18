acff47dba0cc77027ef46c5c3cd75a1a4b9968a0
Denis West <DenisWest@outlook.com>
2023-07-18 02:39:59 -0600

    fix: Removing Strings.xaml from App.xaml

---
+++ SysCredit/Settings/MauiProgram.MacCatalyst.cs
@@ -0,0 +1,24 @@
+﻿namespace SysCredit;
+
+using Maui.FreakyControls.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static partial class MauiProgram
+{
+    private static void ConfigureMauiHandlers(IMauiHandlersCollection Handlers)
+    {
+        Handlers.AddFreakyHandlers(); // To Init your freaky handlers for Entry and Editor
+    }
+
+    public static MauiAppBuilder InitSkiaSharpLibrary(this MauiAppBuilder Builder)
+    {
+        // This line is needed for the following issue: https://github.com/mono/SkiaSharp/issues/1979
+        Builder.InitSkiaSharp(); // Use this if you want to use FreakySvgImageView
+        return Builder;
+    }
+}
