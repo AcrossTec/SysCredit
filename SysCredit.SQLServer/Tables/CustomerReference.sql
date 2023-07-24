c1c9e4e1c1dee993dcaf191dce661a81294c9ac9
Denis West <DenisWest@outlook.com>
2023-07-23 22:15:14 -0600

    feat: Adding Reference Table and Adding Key Entity Settings

---
+++ SysCredit.SQLServer/Tables/CustomerReference.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CustomerReference]
+(
+    [CustomerReferenceId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerId] BIGINT NOT NULL, 
+    [ReferenceId] BIGINT NOT NULL, 
+    [LoanId] BIGINT NULL, 
+    CONSTRAINT [AK_CustomerReference_CustomerId_ReferenceId_LoanId] UNIQUE ([CustomerId], [ReferenceId], [LoanId])
+)
+GO
