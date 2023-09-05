06e71f312dd353fd3c964a0881851de24f370755
wegner01 <kenneth.lola119@est.unanleon.edu.ni>
2023-09-05 10:06:20 -0600

    feat: Get All Complete LoanType Endpoint

--- SysCredit.Api/Interfaces/ILoanTypeService.cs
+++ SysCredit.Api/Interfaces/ILoanTypeService.cs
@@ -1,6 +1,10 @@
 ﻿namespace SysCredit.Api.Interfaces;
 
+using SysCredit.Models;
+
 public interface ILoanTypeService
 {
     IAsyncEnumerable<LoanTypeInfo> FetchLoanTypeAsync();
+
+    IAsyncEnumerable<LoanType> FetchLoanTypeCompleteAsync();
 }
