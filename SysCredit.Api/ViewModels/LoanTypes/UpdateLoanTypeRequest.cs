9f7c1efe67f071026a15c18479dc0f199891e62a
Johan P <johanedfb@gmail.com>
2023-09-07 14:36:58 -0600

    feat: Create Update LoanType Endpoind

---
+++ SysCredit.Api/ViewModels/LoanTypes/UpdateLoanTypeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SysCredit.Api.ViewModels.LoanTypes;
+
+using SysCredit.Api.Attributes;
+using SysCredit.Api.Validations.LoanTypes;
+
+[Validator<UpdateLoanTypeValidator>]
+public class UpdateLoanTypeRequest : IViewModel
+{
+    public long? LoanTypeId { get; set; }
+
+    public string Name { get; set; } = String.Empty;
+}
