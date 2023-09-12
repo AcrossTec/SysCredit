09e1a0701c1b059f13c74565441419d2bbc5a647
Denis West <DenisWest@outlook.com>
2023-09-12 12:53:58 -0600

    fix: Logger fixes

--- SysCredit.Api.Tests/IntegrationTests/SysCreditServerFactory.cs
+++ SysCredit.Api.Tests/IntegrationTests/SysCreditServerFactory.cs
@@ -1,7 +1,23 @@
 ﻿namespace SysCredit.Api.Tests.IntegrationTests;
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+///     Integration tests in ASP.NET Core
+///     https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-7.0
+/// </summary>
+/// <typeparam name="TProgram"></typeparam>
 public class SysCreditServerFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
+    protected override void ConfigureWebHost(IWebHostBuilder Builder)
+    {
+        Builder.ConfigureServices(delegate (WebHostBuilderContext Context, IServiceCollection Services)
+        {
+
+        });
+
+        Builder.UseEnvironment("Development");
+    }
 }
