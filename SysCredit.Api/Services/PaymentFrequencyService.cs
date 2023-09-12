6f95cd40636de87b63c082c9e49860d87e0a1529
Kenneth Lola <kenthsamu@gmail.com>
2023-09-11 22:25:56 -0600

    feat: Get All Complete PaymentFrequency EndPoint

--- SysCredit.Api/Services/PaymentFrequencyService.cs
+++ SysCredit.Api/Services/PaymentFrequencyService.cs
@@ -15,10 +15,27 @@ using static Constants.ErrorCodePrefix;
 [ErrorCodePrefix(PaymentFrequencyServicePrefix)]
 public class PaymentFrequencyService(IStore<PaymentFrequency> PaymentFrequencyStore, ILogger<PaymentFrequencyService> Logger) : IPaymentFrequencyService
 {
+    /// <summary>
+    /// Este método realiza una llamada asincrónica para obtener información
+    /// de frecuencia de pago (DTO)
+    /// </summary>
+    /// <returns></returns>
     [MethodId("F454CC3B-23ED-4A4E-B27E-5E6377CA3B5D")]
     public IAsyncEnumerable<PaymentFrequencyInfo> FetchPaymentFrequencyAsync()
     {
         Logger.LogInformation($"CALL: {nameof(PaymentFrequencyService)}.{nameof(FetchPaymentFrequencyAsync)}");
         return PaymentFrequencyStore.FetchPaymentFrequencyAsync();
+    }
+
+    /// <summary>
+    /// Este método realiza una llamada asincrónica para obtener información
+    /// de frecuencia de pago
+    /// </summary>
+    /// <returns></returns>
+    [MethodId("6006B8FC-E3F7-43E4-9E70-6E6CA69053B0")]
+    public IAsyncEnumerable<PaymentFrequency> FetchPaymentFrequencyCompleteAsync()
+    {
+        Logger.LogInformation($"CALL: {nameof(PaymentFrequencyService)}.{nameof(FetchPaymentFrequencyCompleteAsync)}");
+        return PaymentFrequencyStore.FetchPaymentFrequencyCompleteAsync();
     }
 }
