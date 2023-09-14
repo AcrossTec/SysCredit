7184541db5b57df812b67ad813e01d6259f24f8c
Denis West <DenisWest@outlook.com>
2023-09-13 19:48:46 -0600

    feat: Implementation of unit testing architecture, Part 2

--- SysCredit.Api.Tests/IntegrationTests/LoanTypeEndPointTests.cs
+++ SysCredit.Api.Tests/IntegrationTests/LoanTypeEndPointTests.cs
@@ -1,6 +1,10 @@
 ﻿namespace SysCredit.Api.Tests.IntegrationTests;
 
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+using SysCredit.Helpers;
 
 public class LoanTypeEndPointTests : IClassFixture<SysCreditServerFactory<Program>>
 {
@@ -10,9 +14,55 @@ public class LoanTypeEndPointTests : IClassFixture<SysCreditServerFactory<Progra
     public LoanTypeEndPointTests(SysCreditServerFactory<Program> ServerFactory)
     {
         this.ServerFactory = ServerFactory;
-        this.Client = ServerFactory.CreateClient(new WebApplicationFactoryClientOptions
+
+        this.Client = ServerFactory
+            .WithWebHostBuilder(Builder => Builder.ConfigureTestServices(ConfigureTestServices))
+            .CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+    }
+
+    /// <summary>
+    ///     Inject mock services
+    ///     https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-7.0#inject-mock-services
+    /// </summary>
+    /// <param name="Context"></param>
+    /// <param name="Services"></param>
+    private void ConfigureTestServices(IServiceCollection Services)
+    {
+    }
+
+    public static IEnumerable<LoanTypeInfo> GetLoanTypeInfo()
+    {
+        for (int Index = 1; Index <= 25; ++Index)
         {
-            AllowAutoRedirect = false
+            yield return new LoanTypeInfo { LoanTypeId = Index, Name = $"Name{Index}" };
+        }
+    }
+
+    [Fact]
+    public async Task Get_EndpointReturnLoanTypesTest()
+    {
+        HttpResponseMessage HttpResponse = await Client.GetAsync("/Api/LoanType");
+        HttpResponse.EnsureSuccessStatusCode();
+
+        Assert.Equal("application/json; charset=utf-8", HttpResponse.Content.Headers.ContentType!.ToString());
+
+        var Json = await HttpResponse.Content.ReadAsStringAsync();
+        Assert.NotEmpty(Json);
+
+        var Response = Newtonsoft.Json.JsonConvert.DeserializeObject<ServiceResult<List<LoanTypeInfo>>>(Json);
+
+        Assert.NotNull(Response);
+        Assert.NotNull(Response.Status);
+        Assert.False(Response.Status.HasError);
+        Assert.NotNull(Response.Data);
+        Assert.NotEmpty(Response.Data);
+
+        int Index = 1;
+        Assert.Contains(Response.Data, LoanType =>
+        {
+            ++Index;
+            bool Result = LoanType.LoanTypeId == Index && LoanType.Name == $"Name{Index}";
+            return Result;
         });
     }
 }
---
+++ SysCredit.Api/Patchers/Stores/LoanTypeStoreFetchLoanTypeAsyncPatcher.cs
@@ -0,0 +1,45 @@
+﻿namespace SysCredit.Api.Patchers.Stores;
+
+using HarmonyLib;
+
+using SysCredit.Api.Stores;
+using SysCredit.Models;
+
+/// <summary>
+///     Annotations
+///     https://harmony.pardeike.net/articles/annotations.html
+/// </summary>
+/// <remarks>
+///     Patching: Auxilary patch methods
+///     https://harmony.pardeike.net/articles/patching-auxilary.html
+/// </remarks>
+[HarmonyPatch(typeof(LoanTypeStore))]
+[HarmonyPatch(nameof(LoanTypeStore.FetchLoanTypeAsync))]
+[HarmonyPatch(new Type[] { typeof(IStore<LoanType>) })]
+public class LoanTypeStoreFetchLoanTypeAsyncPatcher
+{
+    private static Func<IAsyncEnumerable<LoanTypeInfo>>? PrefixFetchLoanTypeAsync;
+
+    internal static void SetFetchLoanTypeAsync(Func<IAsyncEnumerable<LoanTypeInfo>> Func)
+    {
+        PrefixFetchLoanTypeAsync = Func;
+    }
+
+    /// <summary>
+    ///     Patching: Prefix
+    ///     https://harmony.pardeike.net/articles/patching-prefix.html
+    /// </summary>
+    public static bool Prefix(ref IAsyncEnumerable<LoanTypeInfo>? __result)
+    {
+        __result = PrefixFetchLoanTypeAsync?.Invoke();
+        return false; // Skips the original method
+    }
+
+    /// <summary>
+    ///     Patching: Postfix
+    ///     https://harmony.pardeike.net/articles/patching-prefix.html
+    /// </summary>
+    public static void Postfix(ref IAsyncEnumerable<LoanTypeInfo> __result)
+    {
+    }
+}
