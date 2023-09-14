fc22f93eee4837dcc007aba48d29428180a592f9
Denis West <DenisWest@outlook.com>
2023-09-14 10:59:06 -0600

    feat: Add error code generator project

---
+++ SysCredit.Api.Generators/ErrorCodeGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace SysCredit.Api.Generators;
+
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public class ErrorCodeGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext Context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext Context)
+    {
+    }
+}
