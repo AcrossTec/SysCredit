2843a43e658a35aff461cd8e8ff7ad08b29956dc
Denis West <DenisWest@outlook.com>
2023-09-13 19:48:46 -0600

    feat: Implementation of unit testing architecture, Part 2

--- SysCredit.Api.Tests/IntegrationTests/SysCreditServerFactory.cs
+++ SysCredit.Api.Tests/IntegrationTests/SysCreditServerFactory.cs
@@ -2,22 +2,24 @@
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
 ///     Integration tests in ASP.NET Core
 ///     https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-7.0
+///     
+///     Testing Controllers with Unit Tests and Moq in ASP.NET Core
+///     https://code-maze.com/unit-testing-controllers-aspnetcore-moq/
+///     
+///     Integration Testing in ASP.NET Core
+///     https://code-maze.com/aspnet-core-integration-testing/
 /// </summary>
 /// <typeparam name="TProgram"></typeparam>
 public class SysCreditServerFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
     protected override void ConfigureWebHost(IWebHostBuilder Builder)
     {
-        Builder.ConfigureServices(delegate (WebHostBuilderContext Context, IServiceCollection Services)
-        {
-
-        });
-
         Builder.UseEnvironment("Development");
     }
 }
---
+++ SysCredit.Api/Patchers/SysCreditApiPatcher.cs
@@ -0,0 +1,23 @@
+﻿namespace SysCredit.Api.Patchers;
+
+using HarmonyLib;
+
+/// <summary>
+///     Introduction
+///     https://harmony.pardeike.net/articles/intro.html
+/// </summary>
+internal static class SysCreditApiPatcher
+{
+    private static Harmony? Harmony;
+
+    public static void PatchAll(string HarmonyId = nameof(SysCreditApiPatcher))
+    {
+        Harmony = new Harmony(HarmonyId);
+        Harmony.PatchAll(typeof(SysCreditApiPatcher).Assembly);
+    }
+
+    public static void UnpatchAll()
+    {
+        Harmony?.UnpatchAll(Harmony.Id);
+    }
+}
