1e891c53ce6d733ffdb391fac9acbc9901de9b4e
Lawrence Rugama <lawrenrugama@gmail.com>
2023-09-18 15:01:44 -0600

    feat: Create Get CustomerId/Loans

---
+++ SysCredit.SQLServer/StoredProcedures/FetchLoansByCustomerId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[FetchLoansByCustomerId] @CustomerId BIGINT
+AS
+BEGIN
+   Select LT.[Name] From [dbo].[Loan] AS L
+   INNER JOIN [dbo].[LoanType] AS LT
+   On L.[LoanTypeId] = LT.[LoanTypeId]
+   INNER JOIN [dbo].[Customer] AS C
+   ON L.[CustomerId] = C.[CustomerId]
+    WHERE C.[CustomerId] = 1;
+END
+GO
