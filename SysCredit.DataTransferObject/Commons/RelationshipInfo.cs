c77b053d19423087e57065f184e5e5f5d69d92d7
dayanaloiretgonzalez <dayanaloiretgonzalez@outlook.com>
2023-10-04 13:18:31 -0600

    feat: Create Endpoint Get Relationship By LoanTypeId

--- SysCredit.DataTransferObject/Commons/RelationshipInfo.cs
+++ SysCredit.DataTransferObject/Commons/RelationshipInfo.cs
@@ -5,4 +5,11 @@ public record class RelationshipInfo : IDataTransferObject
     public long RelationshipId { get; set; }
 
     public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public bool IsEdit { get; set; }
+    public bool IsDelete { get; set; }
+
+
 }
---
+++ SysCredit.SQLServer/StoredProcedures/Catalogs/Relationships/FetchRelationshipByLoanTypeId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[FetchRelationshipByLoanTypeId] 
+@LoanTypeId BIGINT
+AS BEGIN
+        SELECT R.* 
+		FROM [dbo].[Relationship] R
+		INNER JOIN [dbo].[CustomerGuarantor] CG ON R.[RelationshipId] = CG.[RelationshipId]
+		INNER JOIN [dbo].[Loan] L ON CG.[LoanId] = L.[LoanId]
+		INNER JOIN [dbo].[LoanType] LT ON L.[LoanTypeId] = LT.[LoanTypeId]
+		WHERE R.[IsDelete] = 0
+		AND LT.[LoanTypeId] = @LoanTypeId
+END
+GO
