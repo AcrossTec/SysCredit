f53079ced09979ebba38431f5a64dcd7311f6366
Lawrence Rugama <60424187+SoulCrow23@users.noreply.github.com>
2023-10-12 00:55:57 -0600

    feat: Create Guarantor Delete Endpoint (#70)

---
+++ SysCredit.Api/Requests/Guarantors/DeleteGuarantorRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SysCredit.Api.Requests.Guarantors;
+
+using SysCredit.Api.Validations.Guarantors;
+using SysCredit.Api.Attributes;
+
+[Validator<DeleteGuarantorValidator>]
+public class DeleteGuarantorRequest : IRequest
+{
+    public long? GuarantorId { get; set; }
+}
