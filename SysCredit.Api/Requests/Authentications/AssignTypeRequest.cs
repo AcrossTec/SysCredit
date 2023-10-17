a1ad0568f26e0c21c76b54ea51d9139fd15ef132
Lawrence Rugama <lawrenrugama@gmail.com>
2023-10-17 07:39:20 -0600

    add comments for the order authentication & customer

--- SysCredit.Api/Requests/Authentications/AssignTypeRequest.cs
+++ SysCredit.Api/Requests/Authentications/AssignTypeRequest.cs
@@ -1,6 +1,12 @@
 ﻿namespace SysCredit.Api.Requests.Authentications;
 
+/// <summary>
+///     Request del tipo de asignación.
+/// </summary>
 public class AssignTypeRequest : IRequest
 {
+    /// <summary>
+    ///     Nombre del tipo de Asignación que se espera de la url.
+    /// </summary>
     public string RoleName { get; set; } = string.Empty;
 }
