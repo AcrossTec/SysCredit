66da0a6281ed6ebe291d73dd74a2c32f16753b4e
Lael Medina <125894672+LaelMedina@users.noreply.github.com>
2023-10-19 15:05:41 -0600

    feat: Adding Comments to LoanType and PaymentFrequency Validators (#114)

--- SysCredit.Api/Validations/LoanTypes/AsyncLoanTypeUniqueNameValidator.cs
+++ SysCredit.Api/Validations/LoanTypes/AsyncLoanTypeUniqueNameValidator.cs
@@ -13,18 +13,26 @@ using System.Threading;
 using System.Threading.Tasks;
 
 /// <summary>
-///     Clase validadora del nombre del Request para crear un tipo de prestamo
+///     Clase validadora del nombre del Request para crear un tipo de préstamo.
 /// </summary>
 /// <typeparam name="T">Tipo del store</typeparam>
 public class AsyncLoanTypeUniqueNameValidator<T> : AsyncPropertyValidator<T, string?>
 {
     /// <summary>
-    ///     Valida que el nombre sea unico
+    ///     Valida que el nombre sea único.
     /// </summary>
-    /// <param name="Context">Obtiene el objeto donde fue usado el validador</param>
-    /// <param name="Name">Nombre del tipo de prestamo</param>
-    /// <param name="Cancellation">Metodo para cancelar la validación</param>
-    /// <returns></returns>
+    /// <param name="Context">
+    ///     Obtiene el objeto donde fue usado el validador.
+    /// </param>
+    /// <param name="Name">
+    ///     Nombre del tipo de préstamo.
+    /// </param>
+    /// <param name="Cancellation">
+    ///     Método para cancelar la validación.
+    /// </param>
+    /// <returns>
+    ///     Retorna true si el LoanType no existe.
+    /// </returns>
     public override async Task<bool> IsValidAsync(ValidationContext<T> Context, string? Name, CancellationToken Cancellation)
     {
         var LoanType = await Context.RootContextData[nameof(LoanTypeStore)].AsStore<LoanType>().FetchLoanTypeByName(Name);
@@ -32,17 +40,21 @@ public class AsyncLoanTypeUniqueNameValidator<T> : AsyncPropertyValidator<T, str
     }
 
     /// <summary>
-    ///     Retorna el mensaje de error
+    ///     Lanza el error del validador.
     /// </summary>
-    /// <param name="ErrorCode">Codigo del mensaje de error</param>
-    /// <returns></returns>
+    /// <param name="ErrorCode">
+    ///     Código del mensaje de error.
+    /// </param>
+    /// <returns>
+    ///     Retorna el mensaje de error.
+    /// </returns>
     protected override string GetDefaultMessageTemplate(string ErrorCode)
     {
         return ErrorCodeMessages.GetMessageFromCode(ErrorCode)!;
     }
 
     /// <summary>
-    ///     Nombre del validador
+    ///     Nombre del validador.
     /// </summary>
     public override string Name => "AsyncLoanTypeUniqueNameValidator";
 }
