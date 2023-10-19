33bfadeb16c90b0278213b980937865f9d6c4490
Loiret González Avilés <145065301+dayanaloiretgonzalez@users.noreply.github.com>
2023-10-19 16:21:20 -0600

    feat: Add Comments to Guarantors and LoanTypes Request (#113)

--- SysCredit.Api/Requests/LoanTypes/UpdateLoanTypeRequest.cs
+++ SysCredit.Api/Requests/LoanTypes/UpdateLoanTypeRequest.cs
@@ -5,14 +5,20 @@ using SysCredit.Api.Attributes;
 using SysCredit.Api.Validations.LoanTypes;
 
 /// <summary>
-///     Request del actualizador de tipo de prestamo
+///     Request para actualizar el tipo de préstamo con un nombre único.
 /// </summary>
 [Validator<UpdateLoanTypeValidator>]
 public class UpdateLoanTypeRequest : IRequest
 {
+    /// <summary>
+    ///     Id del tipo de préstamo que se desea actualizar proveniente de la ruta.
+    /// </summary>
     [FromRoute]
     public long LoanTypeId { get; set; }
 
+    /// <summary>
+    ///     Nuevo nombre del tipo de préstamo obtenido del cuerpo de la solicitud.
+    /// </summary>
     [FromBody]
     public string Name { get; set; } = String.Empty;
 }
