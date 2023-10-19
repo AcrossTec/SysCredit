9b8f32c40990388ed277f26039153028b590efc6
Lael Medina <125894672+LaelMedina@users.noreply.github.com>
2023-10-19 14:04:42 -0600

    feat: Adding Comments to PaymentFrequency, Reference and Relationship Requests (#112)

--- SysCredit.Api/Requests/Relationships/CreateRelationshipRequest.cs
+++ SysCredit.Api/Requests/Relationships/CreateRelationshipRequest.cs
@@ -3,8 +3,14 @@
 using SysCredit.Api.Attributes;
 using SysCredit.Api.Validations.Relationships;
 
+/// <summary>
+///     Request para crear un parentesco entre el cliente y el fiador.
+/// </summary>
 [Validator<CreateRelationshipValidator>]
 public class CreateRelationshipRequest : IRequest
 {
+    /// <summary>
+    ///     Propiedad que representa el nombre del parentesco.
+    /// </summary>
     public string? Name { get; set; } = string.Empty;
 }
--- SysCredit.Api/Requests/Relationships/UpdateRelationshipRequest.cs
+++ SysCredit.Api/Requests/Relationships/UpdateRelationshipRequest.cs
@@ -6,19 +6,19 @@ using SysCredit.Api.Attributes;
 using SysCredit.Api.Validations.Relationships;
 
 /// <summary>
-/// 
+///     Request para actualizar un parentesco entre el cliente y el fiador.
 /// </summary>
 [Validator<UpdateRelationshipValidator>]
 public class UpdateRelationshipRequest : IRequest
 {
     /// <summary>
-    /// 
+    ///     Propiedad que representa el Id del parentesco.
     /// </summary>
     [FromRoute]
     public long RelationshipId { get; set; }
 
     /// <summary>
-    /// 
+    ///     Propiedad que representa el nombre del parentesco.
     /// </summary>
     [FromBody]
     public string Name { get; set; } = String.Empty;
