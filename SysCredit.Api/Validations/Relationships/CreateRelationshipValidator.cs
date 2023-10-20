f8a8a7afa14868778f72efcd977fe00c71f985e8
Loiret González Avilés <145065301+dayanaloiretgonzalez@users.noreply.github.com>
2023-10-20 14:12:20 -0600

    feat: Add Comments to References and Relationships Validator (#115)

--- SysCredit.Api/Validations/Relationships/CreateRelationshipValidator.cs
+++ SysCredit.Api/Validations/Relationships/CreateRelationshipValidator.cs
@@ -5,8 +5,14 @@ using FluentValidation;
 using SysCredit.Api.Extensions;
 using SysCredit.Api.Requests.Relationships;
 
+/// <summary>
+///     Clase validadora de <see cref="CreateRelationshipRequest"/>.
+/// </summary>
 public class CreateRelationshipValidator : AbstractValidator<CreateRelationshipRequest>
 {
+    /// <summary>
+    ///     Valida el nombre de la relación de parentesco.
+    /// </summary>
     public CreateRelationshipValidator()
     {
         RuleFor(R => R.Name)
