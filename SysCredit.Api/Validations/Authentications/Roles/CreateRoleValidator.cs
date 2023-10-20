2627d50941542b1e18ecae0a42d8f083e913c00d
Lawrence Rugama <60424187+SoulCrow23@users.noreply.github.com>
2023-10-20 10:06:48 -0600

    feat: Add Comments to Customer and Authentication Validator (#111)

--- SysCredit.Api/Validations/Authentications/Roles/CreateRoleValidator.cs
+++ SysCredit.Api/Validations/Authentications/Roles/CreateRoleValidator.cs
@@ -5,8 +5,14 @@ using FluentValidation;
 using SysCredit.Api.Extensions;
 using SysCredit.Api.Requests.Authentications.Roles;
 
+/// <summary>
+///     Clase Validadora de <see cref="CreateRoleRequest"/>.
+/// </summary>
 public class CreateRoleValidator : AbstractValidator<CreateRoleRequest>
 {
+    /// <summary>
+    ///     Valida el Nombre del Rol a Crear.
+    /// </summary>
     public CreateRoleValidator()
     {
         RuleFor(R => R.Name)
--- SysCredit.Api/Validations/Authentications/Users/AsyncUserUniquePhoneValidator.cs
+++ SysCredit.Api/Validations/Authentications/Users/AsyncUserUniquePhoneValidator.cs
@@ -6,19 +6,37 @@ using SysCredit.Api.Extensions;
 using SysCredit.Api.Stores;
 using SysCredit.Models;
 
+/// <summary>
+///     Clase Validador del Numero del Request para Crear un usuario.
+/// </summary>
+/// <typeparam name="T">Tipo del Store</typeparam>
 public class AsyncUserUniquePhoneValidator<T> : AsyncPropertyValidator<T, string?>
 {
-    public override Task<bool> IsValidAsync(FluentValidation.ValidationContext<T> Context, string? Value, CancellationToken cancellation)
+    /// <summary>
+    ///     Valida que el Usuario tenga Un numero unico de telefono.
+    /// </summary>
+    /// <param name="Context">Obtiene el objeto donde fue usado el validador</param>
+    /// <param name="Value">Valor del Numero unico por usuario</param>
+    /// <param name="Cancellation">Metdo para detener la validacion</param>
+    /// <returns>Retorna Un valor booleano que indica si la validación es exitosa o no.</returns>
+    public override Task<bool> IsValidAsync(FluentValidation.ValidationContext<T> Context, string? Value, CancellationToken Cancellation)
     {
-        // var User = await Context.RootContextData[nameof(UserStore)].AsStore<User>().FetchUserByPhoneAsync(Value);
-        // return User is null;
+       
         return Task.FromResult(false);
     }
 
-    public override string Name => "AsyncUserUniquePhoneValidator";
-
+    /// <summary>
+    ///     Retorna el mensaje de Error.
+    /// </summary>
+    /// <param name="ErrorCode">Codigo del mensaje de Error</param>
+    /// <returns>Retorna una cadena de texto que contiene el mensaje de error.</returns>
     protected override string GetDefaultMessageTemplate(string ErrorCode)
     {
         return "'{PropertyName}' Ya existe un registro con este valor.";
     }
+
+    /// <summary>
+    ///     Nombre del Validador
+    /// </summary>
+    public override string Name => "AsyncUserUniquePhoneValidator";
 }
