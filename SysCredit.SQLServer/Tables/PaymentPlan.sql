1a8e992e42233ba56fa0e4f48488b4c66c81f897
Lael Medina <laelmedina90@gmail.com>
2023-10-25 08:06:03 -0600

    feat: Formatting Loan, PaymentFrequency, LoanType And PaymentPlanDetails Tables

--- SysCredit.SQLServer/Tables/PaymentPlan.sql
+++ SysCredit.SQLServer/Tables/PaymentPlan.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [dbo].[PaymentPlan]
 (
-    [PaymentPlanId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [CustomerId] BIGINT NOT NULL, 
-    [LoanId] BIGINT NOT NULL, 
+    [PaymentPlanId]  BIGINT         NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerId]     BIGINT         NOT NULL, 
+    [LoanId]         BIGINT         NOT NULL, 
     [InitialBalance] DECIMAL(22, 4) NOT NULL, 
-    [CreatedDate] DATETIME2 NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-    [ModifiedDate] DATETIME2 NULL, 
-    [DeletedDate] DATETIME2 NULL, 
-    [IsEdit] BIT NOT NULL DEFAULT 0 , 
-    [IsDelete] BIT NOT NULL DEFAULT 0, 
-    CONSTRAINT [AK_PaymentPlan_CustomerId_LoanId] UNIQUE ([CustomerId], [LoanId]), 
-    CONSTRAINT [FK_PaymentPlan_LoanId_Loan_LoanId] FOREIGN KEY ([LoanId]) REFERENCES [Loan]([LoanId]), 
+    [CreatedDate]    DATETIME2      NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    [ModifiedDate]   DATETIME2      NULL, 
+    [DeletedDate]    DATETIME2      NULL, 
+    [IsEdit]         BIT            NOT NULL DEFAULT 0 , 
+    [IsDelete]       BIT            NOT NULL DEFAULT 0, 
+    CONSTRAINT [AK_PaymentPlan_CustomerId_LoanId]              UNIQUE ([CustomerId], [LoanId]), 
+    CONSTRAINT [FK_PaymentPlan_LoanId_Loan_LoanId]             FOREIGN KEY ([LoanId])     REFERENCES [Loan]([LoanId]), 
     CONSTRAINT [FK_PaymentPlan_CustomerId_Customer_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([CustomerId])
 )
 GO
