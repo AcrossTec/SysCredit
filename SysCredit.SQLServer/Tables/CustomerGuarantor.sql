cc12c6c31b5459539f06a39f5cc66814bb4336b5
Loiret González Avilés <145065301+dayanaloiretgonzalez@users.noreply.github.com>
2023-10-26 10:14:39 -0600

    feat: Add Foreign Keys to CustomerReference and CustomerGuarantor Table (#130)

--- SysCredit.SQLServer/Tables/CustomerGuarantor.sql
+++ SysCredit.SQLServer/Tables/CustomerGuarantor.sql
@@ -14,6 +14,7 @@
     CONSTRAINT [FK_CustomerGuarantor_CustomerId_Customer_CustomerId]             FOREIGN KEY ([CustomerId])     REFERENCES [Customer]([CustomerId]), 
     CONSTRAINT [FK_CustomerGuarantor_GuarantorId_Guarantor_GuarantorId]          FOREIGN KEY ([GuarantorId])    REFERENCES [Guarantor]([GuarantorId]), 
     CONSTRAINT [FK_CustomerGuarantor_RelationshipId_Relationship_RelationshipId] FOREIGN KEY ([RelationshipId]) REFERENCES [Relationship]([RelationshipId]),
+    CONSTRAINT [FK_CustomerGuarantor_LoanId_Loan_LoanId]                         FOREIGN KEY ([LoanId])         REFERENCES [Loan]([LoanId]), 
     CONSTRAINT [AK_CustomerGuarantor_CustomerId_GuarantorId_LoanId]              UNIQUE ([CustomerId], [GuarantorId], [LoanId]), 
 )
 GO
