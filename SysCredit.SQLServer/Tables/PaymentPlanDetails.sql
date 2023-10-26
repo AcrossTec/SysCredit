febfcd91c77b46c28da717ee69bb0260567222fa
Lael Medina <laelmedina90@gmail.com>
2023-10-26 14:16:54 -0600

    feat: Formatting Loan, LoanType, PaymentFrequency, PaymentPlan, PaymentPlanDetails

--- SysCredit.SQLServer/Tables/PaymentPlanDetails.sql
+++ SysCredit.SQLServer/Tables/PaymentPlanDetails.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [dbo].[PaymentPlanDetails]
 (
-    [PaymentPlanDetailId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [PaymentPlanId] BIGINT NOT NULL, 
-    [PaymentDate] DATETIME2 NOT NULL, 
-    [PaymentValue] DECIMAL(22, 4) NOT NULL, 
-    [Balance] DECIMAL(22, 4) NOT NULL, 
-    [CreatedDate] DATETIME2 NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-    [ModifiedDate] DATETIME2 NULL, 
-    [DeletedDate] DATETIME2 NULL, 
-    [IsEdit] BIT NOT NULL DEFAULT 0 , 
-    [IsDelete] BIT NOT NULL DEFAULT 0, 
+    [PaymentPlanDetailId] BIGINT         NOT NULL PRIMARY KEY IDENTITY, 
+    [PaymentPlanId]       BIGINT         NOT NULL, 
+    [PaymentDate]         DATETIME2      NOT NULL, 
+    [PaymentValue]        DECIMAL(22, 4) NOT NULL, 
+    [Balance]             DECIMAL(22, 4) NOT NULL, 
+    [CreatedDate]         DATETIME2      NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    [ModifiedDate]        DATETIME2      NULL, 
+    [DeletedDate]         DATETIME2      NULL, 
+    [IsEdit]              BIT            NOT NULL DEFAULT 0 , 
+    [IsDelete]            BIT            NOT NULL DEFAULT 0, 
     CONSTRAINT [FK_PaymentPlanDetails_PaymentPlanId_PaymentPlan_PaymentPlanId] FOREIGN KEY ([PaymentPlanId]) REFERENCES [PaymentPlan]([PaymentPlanId])
 )
 GO
