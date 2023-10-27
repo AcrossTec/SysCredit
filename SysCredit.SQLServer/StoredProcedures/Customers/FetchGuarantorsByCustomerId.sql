f754cd964b2648376e7fee30c9944612e31a5919
Kenneth Lola <87386788+kenthLol@users.noreply.github.com>
2023-10-27 09:26:21 -0600

    fix: Customer Endpoint Fixed (#104)

--- SysCredit.SQLServer/StoredProcedures/Customers/FetchGuarantorsByCustomerId.sql
+++ SysCredit.SQLServer/StoredProcedures/Customers/FetchGuarantorsByCustomerId.sql
@@ -1,10 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[FetchGuarantorsByCustomerId] @CustomerId BIGINT
 AS BEGIN
-	SELECT G.*
-	FROM [dbo].[Guarantor] AS G
-	INNER JOIN [dbo].[CustomerGuarantor] AS CG
-	ON G.[GuarantorId] = CG.[GuarantorId]
-	WHERE G.[IsDelete] = 0 
-	AND CG.[CustomerId] = @CustomerId
+    SELECT G.*
+    FROM [dbo].[Guarantor] AS G
+    INNER JOIN [dbo].[CustomerGuarantor] AS CG
+    ON G.[GuarantorId] = CG.[GuarantorId]
+    WHERE G.[IsDelete] = 0 
+    AND CG.[CustomerId] = @CustomerId
 END
 GO
