c9f49419db916727e7338dd8c40075d907b55092
Johan <88113215+KaimDev@users.noreply.github.com>
2023-11-15 14:44:41 -0600

    fix: PaymentFrequency Functions From PostgreSql (#183)

--- SysCredit.PostgreSQL/Functions/Catalogs/PaymentFrequencies/FetchPaymentFrequencyById.sql
+++ SysCredit.PostgreSQL/Functions/Catalogs/PaymentFrequencies/FetchPaymentFrequencyById.sql
@@ -2,15 +2,13 @@ CREATE OR REPLACE FUNCTION "public"."FetchPaymentFrequencyById"
 (
     payment_frequency_id BIGINT
 )
-RETURNS "public"."PaymentFrequency"
+RETURNS SETOF "PaymentFrequency"
 LANGUAGE plpgsql
 AS $function$
-DECLARE
-    payment_frequency "PaymentFrequency";
 BEGIN
-    SELECT * INTO payment_frequency 
+    RETURN QUERY
+    SELECT *
     FROM "PaymentFrequency"
     WHERE NOT "IsDelete" AND "PaymentFrequencyId" = payment_frequency_id;
-    RETURN payment_frequency;
 END;
 $function$;
