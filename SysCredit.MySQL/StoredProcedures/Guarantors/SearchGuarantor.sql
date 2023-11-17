1eb244ed5bc99d9f4f9f9a71d44991ce33345d51
Kenneth Lola <87386788+kenthLol@users.noreply.github.com>
2023-11-17 17:25:46 -0600

    feat: Multiline String for SearchGuarantor Stored Procedure in MySQL Implemented (#204)

--- SysCredit.MySQL/StoredProcedures/Guarantors/SearchGuarantor.sql
+++ SysCredit.MySQL/StoredProcedures/Guarantors/SearchGuarantor.sql
@@ -7,19 +7,16 @@ CREATE PROCEDURE `SearchGuarantor`(
     IN  p_Limit  INT
 )
 BEGIN
-    SET @p_Value = p_Value;
-
-    SET @Statement = CONCAT(
-        'SELECT * FROM `Guarantor`',
-        ' WHERE `IsDelete` = 0',
-        ' AND (`Identification` LIKE CONCAT(\'%\', IFNULL(@p_Value, `Identification`), \'%\')',
-        ' OR   `Name`           LIKE CONCAT(\'%\', IFNULL(@p_Value, `Name`          ), \'%\')',
-        ' OR   `LastName`       LIKE CONCAT(\'%\', IFNULL(@p_Value, `LastName`      ), \'%\')',
-        ' OR   `Phone`          LIKE CONCAT(\'%\', IFNULL(@p_Value, `Phone`         ), \'%\')',
-        ' OR   `Email`          LIKE CONCAT(\'%\', IFNULL(@p_Value, `Email`         ), \'%\'))',
-        ' ORDER BY `Name` ASC, `LastName` ASC'
-    );
+    SET @Statement = 'SELECT * FROM `Guarantor`
+         WHERE `IsDelete` = 0
+         AND (`Identification` LIKE CONCAT(\'%\', IFNULL(?, `Identification`), \'%\')
+         OR   `Name`           LIKE CONCAT(\'%\', IFNULL(?, `Name`          ), \'%\')
+         OR   `LastName`       LIKE CONCAT(\'%\', IFNULL(?, `LastName`      ), \'%\')
+         OR   `Phone`          LIKE CONCAT(\'%\', IFNULL(?, `Phone`         ), \'%\')
+         OR   `Email`          LIKE CONCAT(\'%\', IFNULL(?, `Email`         ), \'%\'))
+         ORDER BY `Name` ASC, `LastName` ASC';
     
+    SET @p_Value = p_Value;
     SET @p_Offset = p_Offset;
     SET @p_Limit = p_Limit;
     
@@ -29,7 +26,7 @@ BEGIN
 
     -- Execute the dynamic SQL statement
     PREPARE stmt FROM @Statement;
-    EXECUTE stmt USING @p_Offset, @p_Limit;
+    EXECUTE stmt USING @p_Value, @p_Value, @p_Value, @p_Value, @p_Value, @p_Offset, @p_Limit;
     DEALLOCATE PREPARE stmt;
 END //
 
